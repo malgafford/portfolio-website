d326a6bf24352cf084806a88d97624ceffb1a017
malgafford <malgafford24@gmail.com>
2022-10-29 15:00:37 -0400

    feat added scroll to the top button on project pages

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,23 @@ function menuToggle() {
     x.className = 'navtoggle';
   }
 }
+
+//Scroll to the top button
+
+let button = document.getElementById("scrollBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    button.style.display = "block";
+  } else {
+    button.style.display = "none";
+  }
+}
+
+//when user clicks button, scroll to the top of page
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
